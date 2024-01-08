72d7fd8124316f5ad1e21f9047d1647dc5104a4e
Shahanwaz <shahnawaz@alumeltech.com>
2024-01-08 11:02:09 +0530

    add data

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Question1 from "@/component/Question1";
+
+export default function Home() {
+  return (
+    <div>
+      <Question1 />
+    </div>
+  )
+}
---
+++ my-app/src/component/Question1.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Question1 = () => {
+    return(
+        <div className="flex items-center justify-center">
+            Lakki & Shahanwaz
+        </div>
+    )
+}
+
+export default Question1;
