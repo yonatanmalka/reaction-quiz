28588e1365b22afed0a4677030c9fe1c5cba26e4
lakki5777 <137788317+lakki5777@users.noreply.github.com>
2024-01-10 17:01:15 +0530

    add

--- my-app/src/component/Question17.tsx
+++ my-app/src/component/Question17.tsx
@@ -17,13 +17,13 @@ const Question17: React.FC<QuestionProps> = ({handleNextStep}) => {
     };
 
     return (
-        <div className="md:pl-[10px] relative  px-0">
-            <div className="textDiv mt-[20px]">
-                <h1 className="text-[#000] mb-[40px] text-[20px] md:text-[26px] font-semibold text-center">
+        <div className="md:pl-[10px] mt-[5px] relative  px-0">
+            <div className="textDiv ">
+                <h1 className="text-[#000] mb-[10px] text-[20px] md:text-[26px] font-semibold text-center">
                     Recommanded plan
                 </h1>
             </div>
-            <div className="w-[20px] h-[230px] md:top-[100px] top-[75px] md:left-[18px] left-[7px]  absolute z-10 lineVertical">
+            <div className="w-[20px] h-[200px] md:top-[70px] top-[55px] md:left-[18px] left-[7px]  absolute z-10 lineVertical">
             </div>
             <div className="flex  relative z-20 flex-row gap-[10px]">
                 <div
@@ -31,53 +31,53 @@ const Question17: React.FC<QuestionProps> = ({handleNextStep}) => {
                     1
                 </div>
                 <div className="flex-col flex">
-                    <div className="text-[14px] text-[#979797] font-semibold">
+                    <div className="text-[12px] text-[#979797] font-semibold">
                         current date
                     </div>
-                    <div className="text-[20px] text-[#343434] font-semibold">
+                    <div className="text-[17px] text-[#343434] font-semibold">
                         Step challenge
                     </div>
-                    <div className="text-[14px] text-[#787676] font-normal">
+                    <div className="text-[12px] text-[#787676] font-normal">
                         Get your team excited with a first challenge
                     </div>
                 </div>
 
             </div>
-            <div className="flex flex-row  relative z-20 mt-[20px] gap-[10px]">
+            <div className="flex flex-row  relative z-20 mt-[10px] gap-[10px]">
                 <div
                     className="bg-[#FFC55A] w-[35px] flex justify-center text-[18px] font-bold items-center h-[35px] rounded-full">
                     2
                 </div>
                 <div className="flex-col flex">
-                    <div className="text-[14px] text-[#979797] font-semibold">
+                    <div className="text-[12px] text-[#979797] font-semibold">
                         current date
                     </div>
-                    <div className="text-[20px] text-[#343434] font-semibold">
+                    <div className="text-[17px] text-[#343434] font-semibold">
                         Event                    </div>
-                    <div className="text-[14px] text-[#787676] font-normal">
+                    <div className="text-[12px] text-[#787676] font-normal">
                         Create team event to connect people together
                     </div>
                 </div>
 
-            </div><div className="flex border-b pb-[10px] mt-[20px] relative  z-20 flex-row gap-[10px]">
+            </div><div className="flex border-b pb-[10px] mt-[10px] relative  z-20 flex-row gap-[10px]">
             <div
                 className="bg-[#FFFCF7] w-[35px] flex justify-center text-[18px] font-bold items-center h-[35px] rounded-full">
                 3
             </div>
             <div className="flex-col flex">
-                <div className="text-[14px] text-[#979797] font-semibold">
+                <div className="text-[12px] text-[#979797] font-semibold">
                     current date
                 </div>
-                <div className="text-[20px] text-[#343434] font-semibold">
+                <div className="text-[17px] text-[#343434] font-semibold">
                     Ongoing
                 </div>
-                <div className="text-[14px] text-[#787676] font-normal">
+                <div className="text-[12px] text-[#787676] font-normal">
                     Unlock other app features to elevate team excitment
                 </div>
             </div>
 
            </div>
-            <div className="text-[18px] mt-[15px] text-center text-[#979797] font-normal">
+            <div className="text-[14px] mt-[15px] text-center text-[#979797] font-normal">
                 This strategy designed to maintain long-term engagement and maximize program ROI
             </div>
             <div className="mt-[]">
@@ -85,7 +85,7 @@ const Question17: React.FC<QuestionProps> = ({handleNextStep}) => {
                     Get started
                 </button>
             </div>
-            <div className="text-[18px] mt-[15px] underline text-center text-[#979797] font-normal">
+            <div className="text-[14px] mt-[15px] underline text-center text-[#979797] font-normal">
                 Create only one-time challenge
             </div>
         </div>
