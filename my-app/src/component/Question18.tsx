8f121401a068ae93429cca5e39dcce060bec49df
lakki5777 <137788317+lakki5777@users.noreply.github.com>
2024-01-11 18:49:44 +0530

    add

--- my-app/src/component/Question18.tsx
+++ my-app/src/component/Question18.tsx
@@ -75,7 +75,7 @@ const Question18: React.FC<QuestionProps> = ({handleClick, setData }) => {
                         type="text"
                         id="firstName"
                         name="firstName"
-                        placeholder="Challenge title (optional)"
+                        placeholder="Challenge title"
                         value={firstName}
                         onChange={(e) => setFirstName(e.target.value)}
                         className="mt-1 p-2 border bg-[#EFF3F6]  border-[#EFF3F6] rounded-md w-full"
