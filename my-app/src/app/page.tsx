9af9928fc5531ff087365e604327576560423174
Shahanwaz <shahnawaz@alumeltech.com>
2024-01-11 18:16:49 +0530

    add data

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -74,14 +74,14 @@ const Dailog = () => {
         <main className="flex justify-center items-center">
             <div className={`w-[375px] z-[20] relative bg-white  ${(currentStep === 22 || currentStep === 23) ? '' : 'h-[100vh]'}  ${(currentStep === 22 || currentStep === 23) ? 'p-0' : 'p-[15px]'} overflow-hidden`}>
                 {shouldRenderComponent  && (
-                    <div className={`flex w-[100%] z-20 relative flex-row ${currentStep === 15 ? 'justify-center' : 'justify-between'} items-end `}>
-                        {currentStep !== 15 && (
+                    <div className={`flex w-[100%] z-20 relative flex-row ${(currentStep !== 15 && currentStep !== 7) ? 'justify-center' : 'justify-between'} items-end `}>
+                        {(currentStep !== 15 && currentStep !== 7) && (
                         <button onClick={() => setCurrentStep(currentStep - 1)}>
                             <Image src={Back} alt={'backButton'} width={15} height={14} />
                         </button>
                          )}
                         <Image src={Logo} alt={'logo'} width={103} height={50} />
-                        {currentStep !== 15 && (
+                        {(currentStep !== 15 && currentStep !== 7) && (
                         <div className="text-[#3C8AF0] text-[16px] font-medium">
                             <span className="text-[#3C8AF0]">{currentStep}</span>/<span className="text-[#979797]">22</span>
                         </div>
