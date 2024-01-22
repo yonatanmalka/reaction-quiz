f9c66dd9a5ca7552384695ab2d7adbc7f700c8af
Shanu Vashistha <shanumanishsaraswat@gmail.com>
2024-01-22 19:34:22 +0530

    Facebook Pixel Added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,7 +47,7 @@ export default function RootLayout ({
             }}>
             </script>
             <noscript>
-                <img height="1" width="1" style={{ display: 'none' }}
+                <img alt="facebook pixel logo" height="1" width="1" style={{ display: 'none' }}
                      src="https://www.facebook.com/tr?id={your-pixel-id-goes-here}&ev=PageView&noscript=1"/>
             </noscript>
             {/*End Facebook Meta Pixel*/}
