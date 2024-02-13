3aea8c4ce8986f6f222af8c61773b0867537bdf8
bodyabandura <bandurabogdan@gmail.com>
2024-02-13 16:53:31 +0200

    hide-variables

--- src/utils/stripe.ts
+++ src/utils/stripe.ts
@@ -1,11 +1,11 @@
 import Stripe from 'stripe';
 
-export const stripe_secret_key: string = process.env.SECRET_KEY as string
-export const stripe_public_key: string = process.env.PUBLIC_KEY as string
-export const ONE_WEEK_TRIAL: string = process.env.TRIAL_ONE_WEEK as string
-export const ONE_WEEK_MONTH_SUBSCRIPTION: string = process.env.ONE_WEEK as string
-export const FOUR_WEEK_TRIAL: string = process.env.TRIAL_FOUR_WEEK as string
-export const FOUR_WEEK_MONTH_SUBSCRIPTION: string = process.env.FOUR_WEEK as string
+export const stripe_secret_key: string = process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY as string
+export const stripe_public_key: string = process.env.NEXT_PUBLIC_KEY as string
+export const ONE_WEEK_TRIAL: string = process.env.NEXT_PUBLIC_STRIPE_PRODUCT_1_TRAIL_KEY as string
+export const ONE_WEEK_MONTH_SUBSCRIPTION: string = process.env.NEXT_PUBLIC_STRIPE_PRODUCT_1_KEY as string
+export const FOUR_WEEK_TRIAL: string = process.env.NEXT_PUBLIC_STRIPE_PRODUCT_2_TRIAL_KEY as string
+export const FOUR_WEEK_MONTH_SUBSCRIPTION: string = process.env.NEXT_PUBLIC_STRIPE_PRODUCT_2_KEY as string
 
 const stripe = new Stripe(stripe_secret_key, {
     apiVersion: "2023-10-16"
