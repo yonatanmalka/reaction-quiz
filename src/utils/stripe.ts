ffbddd32aa8c111e3571f0a0f2feee8ce38cf9f7
bodyabandura <bandurabogdan@gmail.com>
2024-02-13 16:33:43 +0200

    hide-variables

--- src/utils/stripe.ts
+++ src/utils/stripe.ts
@@ -1,11 +1,11 @@
 import Stripe from 'stripe';
 
-export const stripe_secret_key = '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'
-export const stripe_public_key = 'pk_live_6QOucH11OV7Y8ruwa1pWhc3E'
-export const ONE_WEEK_TRIAL: string = 'price_1OhrnhFN3wpDa6wtP1F4tzf7'
-export const ONE_WEEK_MONTH_SUBSCRIPTION: string = 'price_1OhrnGFN3wpDa6wt6ATEB1Wz'
-export const FOUR_WEEK_TRIAL: string = 'price_1ObNYJFN3wpDa6wtBuB7vvTC'
-export const FOUR_WEEK_MONTH_SUBSCRIPTION: string = 'price_1ObNYJFN3wpDa6wtPk7wvNnQ'
+export const stripe_secret_key: string = process.env.SECRET_KEY as string
+export const stripe_public_key: string = process.env.PUBLIC_KEY as string
+export const ONE_WEEK_TRIAL: string = process.env.TRIAL_ONE_WEEK as string
+export const ONE_WEEK_MONTH_SUBSCRIPTION: string = process.env.ONE_WEEK as string
+export const FOUR_WEEK_TRIAL: string = process.env.TRIAL_FOUR_WEEK as string
+export const FOUR_WEEK_MONTH_SUBSCRIPTION: string = process.env.FOUR_WEEK as string
 
 const stripe = new Stripe(stripe_secret_key, {
     apiVersion: "2023-10-16"
